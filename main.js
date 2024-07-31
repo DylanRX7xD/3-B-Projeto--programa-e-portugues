dc17d4353c7611d266a13ef5def7908a683dd94d
Dylan Rojas <105813600+DylanRX7xD@users.noreply.github.com>
2024-07-31 08:39:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,59 +7,121 @@ const perguntas = [
     {
         enunciado: "Pergunta 1",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto:"Alternativa 1",
+                afirmacao:"Resultado 1"
+            },
+            
+            {
+                texto:"Alternativa 2",
+                afirmacao:"Resultado 2 "
+            }
+
+            
         ]
     },
 
     {
         enunciado: "Pergunta 2 ",
         alternativas: [
-            "Alternativa 1 ",
-            "Alternativa 2"
+            {
+                texto: "Alternativa 1 ",
+                afirmacao:"Resultado 3 "
+            },
+
+            {
+                texto: "Alternativa 2",
+                afirmacao: "Resultado 4"
+            }
+            
+        
         ]
     },
 
     {
         enunciado: "Pergunta 3",
         alternativas: [
-            "Alternativa 1 ",
-            "Alternativa 2"
+            {
+                texto: "Alternativa 1 ",
+                afirmacao: "Resultado 5"
+            },
+
+            {
+                texto: "Alternativa 2",
+                afirmacao:"Resultado 6"
+            }
+            
         ]
     },
 
     {
         enunciado: "Pergunta 4 ",
         alternativas: [
-            "Alternativa 1 ",
-            "Alternativa 2"
+            {
+                texto:  "Alternativa 1 ",
+                afirmacao: "Resultado 7"
+            },
+            
+            {
+                texto:"Alternativa 2",
+                afirmacao:"Resultado 8"
+            }
+            
         ]
     },
 
     {
         enunciado: "Pergunta 5 ",
         alternativas: [
-            "Alternativa 1 ",
-            "Alternativa 2"
+            {
+                texto:"Alternativa 1 ",
+                afirmacao: "Resultado 9"
+            },
+
+            {
+                texto:"Alternativa 2",
+                afirmacao:"Resultado 10"
+            }            
+            
         ]
     },
  ]
 
  let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostrarPerguntas(){
+    if(atual >= perguntas.length){
+        mostreResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent =""; 
     mostrarAlternativas();
 }
 
 function mostrarAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", ()  => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada)  {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " "; 
+    atual++;
+    mostrarPerguntas();
+}
+
+function mostreResultado (){
+    caixaPerguntas.textContent = " ... ";
+    textoResultado.textContent =    historiaFinal;
+    caixaAlternativas.textContent = "..";
 }
 
 mostrarPerguntas();
