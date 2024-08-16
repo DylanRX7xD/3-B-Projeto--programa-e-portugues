3c1546ee085a6a8301e004d4dcd78b679c85c0be
Dylan Rojas <105813600+DylanRX7xD@users.noreply.github.com>
2024-08-16 20:09:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado =document.querySelector('.texto-resultado');
 const perguntas = [
     {
-        enunciado: "Jõao no decorrer do dia conta sobre Racismo e discriminação social sendo temas e questões que no decorrer do tempo vem afetando milhões de pessoas ao redor do mundo, desde seu ponto de vista como isso afeta e como podemos prevenir ou ajudar?",
+        enunciado: "Jõao no decorrer do dia conta sobre Racismo e discriminação social sendo temas e questões que no decorrer do tempo vem afetando milhões de pessoas ao redor do mundo, desde seu ponto de vista como isso se manifesta na sociedade atualmente?",
         alternativas: [
             {
-                texto:"A falta de educação e consciencia sobre tal perpetua essas divisões e gerando mais conflitos.",
-                afirmacao:"A falta de educação e consciencia sobre tal perpetua essas divisões e gerando mais conflitos. "
+                texto:"A falta de educação e consciencia sobre tal perpetua essas divisões e gerando mais conflitos. Podendo se manifestar através de atitudes preconceituosas, práticas institucionais injustas, e políticas, entre outros.",
+                afirmacao:"Conseguimos aprender com a ajuda de Jõao e de voçe sobre educação e consciencia sobre tal perpetuandos divisões e gerando mais conflitos. Podendo se manifestar através de atitudes preconceituosas, práticas institucionais injustas, e políticas, entre outros. "
             },
             
             {
                 texto:"No decorrer do tempo vem afetando a vida social, atitudes, e comportamentos, saber valorizar e ajudar aqueles que sofrem injustiças são algumas das coisas que podemos fazer para ajudar.",
-                afirmacao:"No decorrer do tempo vem afetando a vida social, atitudes, e comportamentos, saber valorizar e ajudar aqueles que sofrem injustiças são algumas das coisas que podemos fazer para ajudar. "
+                afirmacao:"No decorrer do tempo joão e voçe aprenderom sobre a vida social, afetando atitudes e comportamentos, sabendo valorizar e ajudar aqueles que sofrem injustiças são algumas das coisas que podemos fazer para ajudar. "
             }
 
             
@@ -30,7 +30,7 @@ const perguntas = [
             },
 
             {
-                texto: "Praticamente foram criadas barreiras significativas que influenciam aqueles pessoas que procuram se relacionar, procurar ajuda e aqueles que são o contrario.",
+                texto: "Praticamente foram criadas barreiras significativas que influenciam aqueles pessoas que procuram ajuda sobre o tema,.",
                 afirmacao: "Praticamente foram criadas barreiras significativas que influenciam aqueles pessoas que procuram se relacionar, procurar ajuda e aqueles que são o contrario."
             }
             
@@ -43,44 +43,44 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Conseguimos dizer que não teriamos problema, ja que com o tempo as desigualdades se resolverão. ",
-                afirmacao: "Resultado 5"
+                afirmacao: "Consegue dizer que não teria problema ja que com o tempo as desigualdades se resolverão com o decorrer do tempo positivamente ou negativamente"
             },
 
             {
-                texto: "Alternativa 2",
-                afirmacao:"Resultado 6"
+                texto: " Apesar disso a discriminação enfraquece os laços comunitários e promove divisões sociais, prejudicando a integração e o entendimento entre os diferentes grupos de pessoas que procuram a igualdade.",
+                afirmacao:"Sendo assim a discriminação vai enfraquecendo os laços comunitários e promovendo divisões sociais, prejudicando a integração e o entendimento entre os diferentes grupos."
             }
             
         ]
     },
 
     {
-        enunciado: "Pergunta 4 ",
+        enunciado: " A despeito disso a discriminação social influencia a saúde mental e física das pessoas?",
         alternativas: [
             {
-                texto:  "Alternativa 1 ",
-                afirmacao: "Resultado 7"
+                texto:  " Sim! desde estresse crônico, ansiedade e depressão. Além disso, pode limitar o acesso a cuidados de saúde adequados, contribuindo para mais problemas fisicos e mentais ",
+                afirmacao: "concorda em que desde cedo se não tratarmos podemos sofrer estresse crônico, ansiedade e depressão. Além disso, pode limitar o acesso a cuidados de saúde adequados, contribuindo para mais problemas fisicos e mentais"
             },
             
             {
-                texto:"Alternativa 2",
-                afirmacao:"Resultado 8"
+                texto:" Não! sobre-entendemos que todos são fortes e não influencia na saude mental e fisica ",
+                afirmacao:"Falamos sobre como não influencia na saude mental fisica desde seu ponto de vista, "
             }
             
         ]
     },
 
     {
-        enunciado: "Pergunta 5 ",
+        enunciado: "Com o decorrer do tempo João veio analisando sobre o tema e tendo mais conhecimento para ajudar, Jõao finalizou com sua ultima pergunta. Qual seria algumas formas eficazes de combater o racismo e a discriminação social? ",
         alternativas: [
             {
-                texto:"Alternativa 1 ",
-                afirmacao: "Resultado 9"
+                texto:"Podemos envolver educação e conscientização, promoção de políticas inclusivas, apoio a legislações antidiscriminação, e uma que consideramos muito importante como é o diálogo intercultural. ",
+                afirmacao: "e finalizando concluimos em como ajudar desde incentivar educação e conscientização, promoção de políticas inclusivas, apoio a legislações antidiscriminação, e uma que consideramos muito importante como é o diálogo intercultural."
             },
 
             {
-                texto:"Alternativa 2",
-                afirmacao:"Resultado 10"
+                texto:"Conscientização e sensibilização sendo importante para promover campanhas de conscientização e sensibilização sobre o tema",
+                afirmacao:"finalizamos em que a conscientização e sensibilização são atividades importantes para promover campanhas de conscientização e sensibilização sobre o tema "
             }            
             
         ]
